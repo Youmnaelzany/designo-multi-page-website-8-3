84de1ac36560d3dab84da5772d4b149c48b6ce07
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2025-03-10 01:59:33 +0200

    Update layout structure for Home page to use flexbox for better responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import WebDesign from "@/components/main/WebDesign";
 export default function Home() {
   return (
     <main className="">
-      <section className="grid grid-cols-1 items-center justify-center xl:grid-cols-2 gap-y-6 xl:gap-x-[1.87rem]">
+      <section className="flex flex-col items-center justify-center xl:flex-row xl:justify-around  gap-y-6 xl:gap-x-[1.87rem]">
         <WebDesign />
         <div className="flex flex-col items-center justify-center gap-y-6">
           <AppDesign />
